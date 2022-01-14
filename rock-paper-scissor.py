aae2b23a0a5ff0854291907ec24c60e056991f3a
saurabh-mani <saurabhmani00@gmail.com>
2022-01-14 11:11:49 +0530

    Corrections made and loop run added

--- rock-paper-scissor.py
+++ rock-paper-scissor.py
@@ -5,14 +5,21 @@ option_map = {
     2 : "Paper",
     3 : "Scissor"
 }
-print("1. Rock\n2. Paper\n3. Scissor")
-player = int(input("Enter Your Choice: "))
-computer = random.randrange(1,4)
-print("Player's Choice: "+ option_map[player])
-print("Computer's Choice: "+ option_map[computer])
-if player == computer:
-    print("It's a draw...")
-elif ((player > computer and player != 3) or (player == 3 and computer == 1)):
-    print("Player Wins...")
-else:
-    print("Computer Wins...")
+
+while True:
+    print("1. Rock\n2. Paper\n3. Scissor\n4. Quit")
+    player = int(input("Enter Your Choice: "))
+    if player == 4:
+        break
+    elif player < 1 or player > 4:
+        print("Invalid choice...")
+        continue
+    computer = random.randrange(1,4)
+    print("Player's Choice: "+ option_map[player])
+    print("Computer's Choice: "+ option_map[computer])
+    if player == computer:
+        print("It's a draw...")
+    elif ((player == (computer+1)) or (player == 1 and computer == 3)):
+        print("Player Wins...")
+    else:
+        print("Computer Wins...")
